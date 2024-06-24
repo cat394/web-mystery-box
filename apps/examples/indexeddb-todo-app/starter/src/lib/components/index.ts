12f098e4f5c98dbbf2a25b78dbff6cfca9cee662
Ryo <91322302+cat394@users.noreply.github.com>
2024-06-24 18:42:33 +0900

    feat: Add PageContainer component to lib/components

--- apps/examples/indexeddb-todo-app/starter/src/lib/components/index.ts
+++ apps/examples/indexeddb-todo-app/starter/src/lib/components/index.ts
@@ -1,4 +1,5 @@
 import TodoList from './TodoList.svelte';
 import Table from './Table.svelte';
+import PageContainer from './PageContainer.svelte';
 
-export { TodoList, Table };
+export { TodoList, Table, PageContainer };
