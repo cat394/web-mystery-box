1e74aed637319e960edf0c6bd9953c60cd74fea3
Ryo <91322302+cat394@users.noreply.github.com>
2024-06-24 10:00:57 +0900

    uncomment some types

--- apps/examples/indexeddb-todo-app/starter/src/lib/idb-helpers/index.ts
+++ apps/examples/indexeddb-todo-app/starter/src/lib/idb-helpers/index.ts
@@ -3,27 +3,27 @@
 // 	IDBObjectStoreHelper,
 // 	IDBIndexHelper,
 // } from './main';
-// import type {
-// 	OpenDBHandlers,
-// 	CursorHandlers,
-// 	EssentialFields,
-// 	WriteOperationResult,
-// 	ObjectStoreOperation,
-// 	ObjectStoreOperationResult,
-// 	DBSetting,
-// 	IndexSetting
-// } from './types';
+import type {
+	OpenDBHandlers,
+	CursorHandlers,
+	EssentialFields,
+	WriteOperationResult,
+	ObjectStoreOperation,
+	ObjectStoreOperationResult,
+	DBSetting,
+	IndexSetting
+} from './types';
 
-// export {
+export {
 // 	IDBDatabaseHelper,
 // 	IDBObjectStoreHelper,
 // 	IDBIndexHelper,
-// 	type OpenDBHandlers,
-// 	type CursorHandlers,
-// 	type EssentialFields,
-// 	type WriteOperationResult,
-// 	type ObjectStoreOperation,
-// 	type ObjectStoreOperationResult,
-// 	type DBSetting,
-// 	type IndexSetting
-// };
+	type OpenDBHandlers,
+	type CursorHandlers,
+	type EssentialFields,
+	type WriteOperationResult,
+	type ObjectStoreOperation,
+	type ObjectStoreOperationResult,
+	type DBSetting,
+	type IndexSetting
+};
